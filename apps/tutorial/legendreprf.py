1eca2734fa306e476d612969f033b781f3e32ee9
amitgtx <amitagarwal.17081996@gmail.com>
2019-11-14 10:21:28 -0600

    Adding comments, restructuring methods, and correcting initial secret sharing of key K

--- apps/tutorial/legendreprf.py
+++ apps/tutorial/legendreprf.py
@@ -19,29 +19,7 @@ config = {
     MixinConstants.MultiplyShare: BeaverMultiply(),
 }
 
-
-async def eval(ctx, powers_of_k_shares, X):
-    coeff = [ctx.field(1)]
-
-    for Xi in X:
-        shift_coeff = coeff + [ctx.field(0)]
-        mult_coeff = [ctx.field(0)] + [elem * Xi for elem in coeff]
-        coeff = [i + j for i, j in zip(shift_coeff, mult_coeff)]
-
-    # print(f"[{ctx.myid}] Inside EVAL : ", coeff)
-    # print(f"[{ctx.myid}] Inside EVAL : ", str(powers_of_k_shares))
-
-    y = coeff[len(coeff) - 1]
-    # y = ctx.ShareFuture()
-    B = len(coeff) - 1
-    for i in range(B):
-        y = y + powers_of_k_shares[i] * coeff[B - 1 - i]
-
-    # print(f"[{ctx.myid}] Inside EVAL : ", y)
-
-    return y
-
-
+# Compute [F_K(X)] := [y]^((p-1)/2) through log2 p multiply/squarings
 async def prf(ctx, y: Share):
     
     p = y.v.modulus
@@ -58,16 +36,62 @@ async def prf(ctx, y: Share):
     return res
 
 
+# Offline phase for generating secret-shared powers of K
+async def offline_powers_generation(ctx, K, B):
+
+    # Precompute [K],[K^2],...[K^B]   powers of k for each block
+    powers_of_k_shares = [ctx.Share(K ** i) + ctx.preproc.get_zero(ctx) for i in range(1, B+1)]
+
+    return powers_of_k_shares
+
+
+# Evaluting the prf on a fixed number of field elements X 
+# using precomputed secret-shared powers of K 
+async def eval(ctx, powers_of_k_shares, X):
+    
+
+    # Determing coefficients of (K + X1)(K + X2).....(K + XB)
+    coeff = [ctx.field(1)]
+
+    for Xi in X:
+        shift_coeff = coeff + [ctx.field(0)]
+        mult_coeff = [ctx.field(0)] + [elem * Xi for elem in coeff]
+        coeff = [i + j for i, j in zip(shift_coeff, mult_coeff)]
+
+
+    # Compute [y] where y = (K+X1)(K+X2)....(K+XB) through Local computations
+    # This is a polynomial y = f(K) where the coefficients of f have been
+    # stored in `coeff`, and we have powers of [K] precomputed
+    y = coeff[len(coeff) - 1]   
+    B = len(coeff) - 1
+
+    for i in range(B):
+        y = y + powers_of_k_shares[i] * coeff[B - 1 - i]
+
+
+    fk_x = await prf(ctx, y)
+    print(f"[{ctx.myid}] PRF OK", fk_x, type(fk_x))
+    
+    # return y
+    return fk_x
+
+
+# Verify whether the reconstrcuted MPC result matches the expected
+# result F_K(X) = legendre_p( (K+X1) * (K+X2) * (K+X3)  .... (K+XB)) 
 def verify(ctx, mpcResult, K, X):
 
     Y = ctx.field(1)
     p = Y.modulus
-    exponent = int((p - 1) / 2)
+    
 
+    #Calculating Y = (K + X1) * (K + X2) ...... * (K + XB)
     for Xi in X:
         Y *= (K + Xi)
 
+    
+    #Calculating expectedResult = legendre_p(Y)
     expectedResult = ctx.field(1)
+    exponent = int((p - 1) / 2)
 
     while exponent > 0:
         if(exponent & 1):
@@ -79,28 +103,29 @@ def verify(ctx, mpcResult, K, X):
 
 
 
-
-
 async def prog(ctx):
     
     K = ctx.field(77)  # Secret key
-    B = 6
+    B = 6               # Number of blocks
     _X = [21, 88, 97, 33, 44, 83]  # B=6 random field elements
     X = [ctx.field(Xi) for Xi in _X]
  
-    powers_of_k_shares = [ctx.Share(K ** i) for i in range(1, B+1)]
-
+    powers_of_k_shares = await offline_powers_generation(ctx, K, B)
     print(f"[{ctx.myid}] Precompute OK")
 
 
-    y = await eval(ctx, powers_of_k_shares, X)
-    print(f"[{ctx.myid}] Eval OK", y, type(y))
+    # y = await eval(ctx, powers_of_k_shares, X)
+    # print(f"[{ctx.myid}] Eval OK", y, type(y))
 
 
-    fk_x = await prf(ctx, y)
-    print(f"[{ctx.myid}] PRF OK", fk_x, type(fk_x))
+    fk_x = await eval(ctx, powers_of_k_shares, X)
+    print(f"[{ctx.myid}] Eval OK", fk_x, type(fk_x))
 
 
+    # fk_x = await prf(ctx, y)
+    # print(f"[{ctx.myid}] PRF OK", fk_x, type(fk_x))
+
+    # Open F_k(X) and reconstruct
     FK_x = await fk_x.open()
     print(f"[{ctx.myid}] Opening OK", FK_x, type(FK_x))
 
@@ -117,9 +142,8 @@ async def prog(ctx):
 async def legendrePRF_challenge():
     # Create a test network of 4 nodes (no sockets, just asyncio tasks)
     n, t = 4, 1
-    K = 77  # Secret key
-    B = 6
     pp = FakePreProcessedElements()
+    pp.generate_zeros(10000, n, t)
     pp.generate_triples(10000, n, t)
 
 
@@ -140,16 +164,3 @@ def main():
 if __name__ == "__main__":
     main()
     print("Legendre PRF challenge ran successfully")
-
-
-
-
-
-
-
-
-
-
-
-
-
