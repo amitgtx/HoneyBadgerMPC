8cd34f164600d329272574d2c24fb87358753cb8
amitgtx <amitagarwal.17081996@gmail.com>
2019-11-25 16:11:45 -0500

    Creating new MPC app for auction

--- apps/auction/cda.py
+++ apps/auction/cda.py
@@ -1,3 +1,6 @@
+"""
+hbMPC tutorial 1. Running sample MPC programs in the testing simulator
+"""
 import asyncio
 from honeybadgermpc.mpc import TaskProgramRunner
 from honeybadgermpc.progs.mixins.dataflow import Share
@@ -8,9 +11,10 @@ from honeybadgermpc.utils.typecheck import TypeCheck
 from honeybadgermpc.progs.mixins.share_arithmetic import (
     MixinConstants,
     BeaverMultiply,
-    BeaverMultiplyArrays,
-    Equality
+    BeaverMultiplyArrays
 )
+
+from honeybadgermpc.progs.mixins.share_comparison import MixinConstants, Equality, LessThan
 
 config = {
     MixinConstants.MultiplyShareArray: BeaverMultiplyArrays(),
@@ -19,26 +23,37 @@ config = {
 }
 
 
+async def linear_search(ctx, key: Share, A):
+    
+    for a in A:
+        flag = (await (a == key).open()):
+        # flag = 3
+        if (flag != 0):
+            return a, 1
+    return -1, 0
+
+
+
 async def prog(ctx):
-
-    K1 = 700
-    k1 = ctx.Share(K1) + ctx.preproc.get_zero(ctx)
-    print(f"[{ctx.myid}] k1 = ", k1)
-
-
-    K2 = 700
-    k2 = ctx.Share(K2) + ctx.preproc.get_zero(ctx)
-    print(f"[{ctx.myid}] k2 = ", k2)
+    
+    _A = [10, 30, 50, 70, 90, 150]
+    # A = [ctx.Share(a) + ctx.preproc.get_zero(ctx) for a in _A]
+    A = [ctx.Share(a) for a in _A]
+    # L = [int(a.v) % 10000 for a in A]
+    # print(f"[{ctx.myid}] A = ", L)
 
 
-
-    res = await (k1 == k2).open()
-
-    print(f"[{ctx.myid}] Result = ", res)
-
+    K = 700
+    k = ctx.Share(K) + ctx.preproc.get_zero(ctx)
+    print(f"[{ctx.myid}] Result = ", index, result)
 
 
-async def tutorial_1():
+    index, result = await linear_search(ctx, k, A)
+
+    print(f"[{ctx.myid}] Result = ", index, result)
+
+
+async def continuous_double_auction():
     # Create a test network of 4 nodes (no sockets, just asyncio tasks)
     n, t = 4, 1
     pp = FakePreProcessedElements()
@@ -55,10 +70,10 @@ def main():
     # Run the tutorials
     asyncio.set_event_loop(asyncio.new_event_loop())
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(tutorial_1())
+    loop.run_until_complete(continuous_double_auction())
     # loop.run_until_complete(tutorial_2())
 
 
 if __name__ == "__main__":
     main()
-    print("Tutorial 1 ran successfully")
+    print("Continuous Double Auction ran successfully")
