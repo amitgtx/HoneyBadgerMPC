9b677c2daac1f0ccddc387345202342b0bc61bd7
amitgtx <amitagarwal.17081996@gmail.com>
2019-11-30 17:15:29 -0600

    Improving code documentation

--- apps/legendrePoC/legendreprf.py
+++ apps/legendrePoC/legendreprf.py
@@ -1,19 +1,18 @@
 '''
-This implementation is a PoC to demonstrate how an MPC-firendly PRF 
+This implementation is a PoC to demonstrate how a MPC-firendly PRF 
 can be used for realizing the "Proof of Custody" scheme. In short, 
 Proof of Custody is a way for nodes (called validators) to "prove" 
 that they are really storing a file which they are obligated to 
 store. Prior realizations of this scheme utilized a "mix" function 
-based on SHA256. Future goal is to make use of primitives which allow 
-i) validator pools to be set up in a secure, trustless manner and 
+based on SHA256. Future goal is to make use of primitives which 
+allow: i) validator pools to be set up in a secure, trustless manner, 
 (ii) allow one-party validators to spread their secret across several 
 machines, reducing the risk of secrets getting compromised. In order 
 to meet this goal, it is required that the primitive is MPC-friendly 
 which, unfortunately, is not a property of SHA256. Fortunately, the 
 "mix" function in the Proof of CUstody scheme can be replaced with 
 any PRF. Consequently, it was proposed that Legendre PRF, an 
-MPC-friendly primitive, would be a good candidate for such 
-replacement
+MPC-friendly primitive, would be a good candidate for such replacement
 (https://ethresear.ch/t/using-the-legendre-symbol-as-a-prf-for-the-proof-of-custody/5169)
 
 The setting is such that there are n nodes out of which t might be 
@@ -23,7 +22,7 @@ atleast a group of t+1 nodes are required in-order to reconstruct the
 secret-key K. In order to "prove the custody" of set of B blocks 
 - {X1, X2, .... XB} - which is basically a public dataset of B field 
 elements, the nodes compute the output of legendre PRF function using 
-their secret key-share [k] and the B field elements as input. This 
+their secret key share [k] and the B field elements as input. This 
 output is represented in the following equation:
     
 
@@ -31,24 +30,24 @@ F_[k](X) = legendre_p(([k]+X1) * ([k]+X2) * ([k]+X3)  .... ([k]+XB))
 
 
 Once each node has computed its share of the output, those outputs 
-can be combined in-order to reconstruct the actual output. In a 
-setting where n > 3t, we can use a technique called robust 
-interpolation for such reconstruction. This technique ensures that 
-i)reconstructed output always matches with the expected output 
-ii) nodes which did not submit or submitted incorrect shares of their 
-PRF output are always identified. This identification of malicious 
-behaviour (coupled with a scheme which provides rewards to nodes 
-submitting correct output shares) incentivizes the nodes to perform 
-the MPC computation honestly.
+can be combined to reconstruct the actual output. In a setting where 
+n > 3t, we can use a technique called robust interpolation for such 
+reconstruction. This technique ensures that: i) reconstructed output 
+always matches with the expected output, ii) nodes which did not 
+submit or submitted incorrect shares of their PRF output are always 
+identified. This identification of malicious behaviour (coupled with 
+a scheme which provides rewards to nodes who submit correct output 
+shares) incentivizes the nodes to perform the MPC computation honestly.
 
 
 Below, we outline the protocol that each node follows:
 
 Precompute:
--  [k],[k^2],...[k^B]   powers of k for each block
+-  [k],[k^2],...[k^B] ; powers of k (secret-share of key) for each 
+block
 
-Protocol:
-- Compute [y] where y = (k+X1)(k+X2)....(k+XB) through Local 
+Online computation:
+- Compute [y] where y = (k+X1)(k+X2)....(k+XB) through local 
 computations. This is a polynomial y = f(k) where the coefficients of 
 f can be determined from constants X1,...,XB, and we have powers of 
 [k] precomputed
@@ -56,29 +55,36 @@ f can be determined from constants X1,...,XB, and we have powers of
 - Open F_k(X) and reconstruct
 
 
-The above protocol is implemented inside the prog() function in 2
- different phases:
+In the following code, the above protocol is implemented inside the 
+prog() function in 2 different phases:
 
-- Offline Phase : In this phase, all nodes collectively obtain a (t,n)
+- Offline Phase : In this phase, each node obtains a (t,n)
 secret-sharing of random key K, and then each node computes succesive
 powers of their secret share ([k], [k^2], [k^3], etc) using the
 offline_powers_generation() function
 
-- Online Phase : In this phase, nodes receive a file (represented as
-an array X of B elements) as input, and then run the MPC protocol by
-consuming the preprocessed powers to obtain a secret-sharing fk_x of
-the desired output. The logic for same is present in the eval()
-function. After this, each node reconstructs the desired output FK_x
-using the open() function.
+- Online Phase : In this phase, nodes run a MPC protocol using the 
+public file (represented as an array X of B elements) and the 
+preprocessed powers of secret shared key as input. The logic for same 
+is present in the eval() function. The output of this function is a 
+secret-sharing fk_x of the desired output. After this, each node 
+reconstructs the desired output FK_x using the open() function.
 
 
 This implementation differs from a real-world deployment in the
-following way: The nodes in this demo are "simulated" as async tasks
+following ways:
+
+1.) The nodes in this demo are "simulated" as async tasks
 which execute concurrently on the same system and communicate with
 each other using message passing. However, in the real-world, each
 node would correspond to a validator (a standalone system) and the
 communication would happen over network sockets
 
+2.) In this demo, nodes obtain a share of the predetermined 
+secret-key K which is computed by the honeybadgerMPC system. In the 
+real world, the "one-party validator" node will be holding the 
+secret-key K and will be responsible for distributing shares of it to 
+each of the n nodes in the "validator pool"
 '''
 
 
@@ -176,7 +182,7 @@ async def eval(ctx, powers_of_k_shares, X):
         coeff = [i + j for i, j in zip(shift_coeff, mult_coeff)]
 
 
-    # Compute [y] where y = (K+X1)(K+X2)....(K+XB) through Local computations
+    # Compute [y] where y = (K+X1)(K+X2)....(K+XB) through local computations
     # This is a polynomial y = f(K) where the coefficients of f have been stored in `coeff`, and we have powers of [k] precomputed
     y = coeff[B]   
 
@@ -244,14 +250,14 @@ async def prog(ctx):
 
     # Open F_k(X) and reconstruct
     FK_x = await fk_x.open()
-    print(f"[{ctx.myid}] Output share reconstruction OK")
+    print(f"[{ctx.myid}] Output share reconstruction OK: ", FK_x)
 
 
     # Return the reconstructed value
     return FK_x
 
 
-async def legendrePRF_challenge():
+async def legendrePRF_proofOfCustody():
     
     program_runner = TaskProgramRunner(n, t, config)
     program_runner.add(prog)
@@ -269,7 +275,7 @@ def main():
 
     asyncio.set_event_loop(asyncio.new_event_loop())
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(legendrePRF_challenge())
+    loop.run_until_complete(legendrePRF_proofOfCustody())
 
 
 if __name__ == "__main__":
